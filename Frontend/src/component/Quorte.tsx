37a42b77babd969854a9b014ed8e2f857965b966
kishalaymodak <kishalaymodak@gmail.com>
2024-03-17 00:54:14 +0530

    Quort change

--- Frontend/src/component/Quorte.tsx
+++ Frontend/src/component/Quorte.tsx
@@ -3,12 +3,20 @@ function Quorte() {
     <div className=" bg-slate-200 flex items-center justify-center h-screen w-full ">
       <div className=" w-4/5  flex flex-col items-start">
         <div className="text-3xl font-bold ">
-          "The customer service i recived was exceptional.The support team went
-          above and beond to address my concerns."
+          "Welcome to Medium, your destination for thought-provoking articles,
+          insightful perspectives, and engaging narratives. As avid believers in
+          the power of ideas, we've created a platform that fosters intellectual
+          exchange, encourages meaningful discussions, and celebrates the
+          diversity of voices within our community. Whether you're a seasoned
+          writer, a curious reader, or simply someone who loves exploring new
+          concepts, you'll find a home here amidst our rich tapestry of content.
+          Join us on a journey of discovery, enlightenment, and inspiration as
+          we navigate the ever-evolving landscape of ideas together. Welcome to
+          a place where words matter. Welcome to Medium."
         </div>
         <div className=" text-2xl font-bold mt-2">Jules Winnfield</div>
         <div className=" text-lg font-semibold text-slate-500">
-          CEO,Acme Inc
+          CEO,Kishalay Modak
         </div>
       </div>
     </div>
