2624e6e3b4169a2524fdcdc73ec40fcc6738a30a
kishalaymodak <kishalaymodak@gmail.com>
2024-03-17 09:46:52 +0530

    hashroute added

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import Blog from "./pages/Blog";
